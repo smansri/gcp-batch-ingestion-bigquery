f0d43c14c4fa5ab60ff9e33dbe38be88aafc11cc
Seifeddin MANSRI <mansri.seifeddine@gmail.com>
2019-04-05 00:55:31 +0200

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,18 +1,18 @@
 terraform {
   backend "gcs" {
-    bucket = "tf-state-gcp-batch-ingestion"
-    region = "europe-west1"
+    bucket = "tf-state-gcp-batch-ingestion-sma"
+    region = "europe-west3"
     prefix = "terraform/state"
   }
 }
 
 provider "google" {
   project = "cloud-proxy-sql-showcase"
-  region = "europe-west1"
+  region = "europe-west3"
 }
 
 resource "google_storage_bucket" "funky-bucket" {
   name = "batch-pipeline"
   storage_class = "REGIONAL"
-  location  = "europe-west1"
+  location  = "europe-west3"
 }
